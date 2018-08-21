9a0e83867499257efb2cdaa9849d9b3945ad4a76
ARK <alok.kesari@yahoo.co.in>
2018-08-21 14:28:33 +0530

    Edited

--- cp_automa.sh
+++ cp_automa.sh
@@ -1,9 +1,11 @@
+set -e
 present=`pwd`
-echo 'Type the name of the Module:'
-read modulename
-mkdir $modulename
-echo '//Type your verilog code here' > `echo $present`/`echo $modulename`/`echo $modulename`.v  
-echo '//Type your input and output constraints here' > `echo $present`/`echo $modulename`/`echo $modulename`.pcf
-sudo cp `echo $present`/automa.sh `echo $present`/`echo $modulename`/automa.sh
-cd `echo $present`/`echo $modulename`
-sudo geany `echo $modulename`.v `echo $modulename`.pcf 
+echo 'Type the name of the module:'
+read modname
+mkdir `echo $present`/`echo $modname`
+echo '//Type your verilog code here' > `echo $present`/`echo $modname`/`echo $modname`.v  
+echo '//Type your input and output constraints here' > `echo $present`/`echo $modname`/`echo $modname`.pcf
+sudo cp `echo $present`/automa.sh `echo $present`/`echo $modname`/automa.sh
+cd `echo $present`/`echo $modname`
+echo 'Stopping this terminal process while editing the Verilog code will kill the Geany editor process. Open another terminal to run other commands while still editing.'
+sudo geany `echo $present`/`echo $modname`/`echo $modname`.v `echo $present`/`echo $modname`/`echo $modname`.pcf 
