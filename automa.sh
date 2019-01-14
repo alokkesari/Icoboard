d2c291834439d32a0ae56e002801cca472946f0b
alokkesari <arkesari@iith.ac.in>
2019-01-14 12:43:43 +0530

    bug fixes

--- automa.sh
+++ automa.sh
@@ -3,9 +3,8 @@ echo 'Type the name of the module:'
 read parent
 echo 'For cases where there are many submodules, type the name of the top module (same as module name otherwise):'
 read topmod
-sudo rm `echo $dir`/`echo $parent`/`echo $parent`.asc `echo $dir`/`echo $parent`/`echo $parent`.bin `echo $dir`/`echo $parent`/`echo $parent`.blif `echo $dir`/`echo $parent`/`echo $parent`.dot `echo $dir`/`echo $parent`/`echo $parent`.pdf 
-sudo rm /root/.yosys_show.dot /root/.yosys_history /root/.yosys_show.dot.pid /root/.yosys_show.ps
-sudo rm `echo $HOME`/yosys/BitMake/ascript.sh
+sudo rm -f `echo $dir`/`echo $parent`/`echo $parent`.asc `echo $dir`/`echo $parent`/`echo $parent`.bin `echo $dir`/`echo $parent`/`echo $parent`.blif `echo $dir`/`echo $parent`/`echo $parent`.dot `echo $dir`/`echo $parent`/`echo $parent`.pdf 
+sudo rm -f `echo $HOME`/yosys/BitMake/ascript.sh
 echo -n 'read_verilog ' >> `echo $HOME`/yosys/BitMake/ascript.sh 
 echo -n `echo $dir` >> `echo $HOME`/yosys/BitMake/ascript.sh
 echo -n '/' >> `echo $HOME`/yosys/BitMake/ascript.sh
@@ -40,7 +39,7 @@ echo '	icoprog -p < $(v_fname).bin' >> `echo $HOME`/yosys/BitMake/Makefile
 cd `echo $HOME`/yosys/BitMake
 sudo `echo $HOME`/yosys/./yosys -p 'script ascript.sh'
 cd `echo $dir`
-sudo rm `echo $HOME`/yosys/BitMake/test.c
+sudo rm -f `echo $HOME`/yosys/BitMake/test.c
 echo '#include<wiringPi.h>' >> `echo $HOME`/yosys/BitMake/test.c
 echo '#include<stdio.h>' >> `echo $HOME`/yosys/BitMake/test.c
 echo 'int main (void)' >> `echo $HOME`/yosys/BitMake/test.c
@@ -53,38 +52,36 @@ echo '{' >> `echo $HOME`/yosys/BitMake/test.c
 echo 't=digitalRead(27);' >> `echo $HOME`/yosys/BitMake/test.c
 echo 'if(t==HIGH)' >> `echo $HOME`/yosys/BitMake/test.c
 echo '{' >> `echo $HOME`/yosys/BitMake/test.c
-echo -n 'printf("<---------------------------Bitstream Upload Completed!------------------------>\n<------------------------------------------------------------------------------>\n<---------------------------FOR THE LOGIC DIAGRAM, TYPE:----------------------->\n<------------------------------------------------------------------------------>\nsudo qpdfview ' >> `echo $HOME`/yosys/BitMake/test.c
+echo -n 'printf("<---------------------------Bitstream Upload Completed!------------------------>\\n<------------------------------------------------------------------------------>\\n<---------------------------FOR THE LOGIC DIAGRAM, TYPE:----------------------->\\n<------------------------------------------------------------------------------>\\nsudo qpdfview ' >> `echo $HOME`/yosys/BitMake/test.c
 echo -n `echo $dir` >> `echo $HOME`/yosys/BitMake/test.c
 echo -n '/' >> `echo $HOME`/yosys/BitMake/test.c
 echo -n `echo $parent` >> `echo $HOME`/yosys/BitMake/test.c
 echo -n '/' >> `echo $HOME`/yosys/BitMake/test.c
 echo -n `echo $parent` >> `echo $HOME`/yosys/BitMake/test.c
-echo -n '.pdf\n<------------------------------------------------------------------------------>\n<-----------------------------------OR CHECK:---------------------------------->\n<------------------------------------------------------------------------------>\n' >> `echo $HOME`/yosys/BitMake/test.c
+echo -n '.pdf\\n<------------------------------------------------------------------------------>\\n<-----------------------------------OR CHECK:---------------------------------->\\n<------------------------------------------------------------------------------>\\n' >> `echo $HOME`/yosys/BitMake/test.c
 echo -n `echo $dir` >> `echo $HOME`/yosys/BitMake/test.c
 echo -n '/' >> `echo $HOME`/yosys/BitMake/test.c
 echo -n `echo $parent` >> `echo $HOME`/yosys/BitMake/test.c
-echo '\n<------------------------------------------------------------------------------>\n");}' >> `echo $HOME`/yosys/BitMake/test.c
+echo '\\n<------------------------------------------------------------------------------>\\n");}' >> `echo $HOME`/yosys/BitMake/test.c
 echo 'else if(t==LOW)' >> `echo $HOME`/yosys/BitMake/test.c
 echo '{' >> `echo $HOME`/yosys/BitMake/test.c
-echo 'printf("<---------------------------Bitstream Upload Failed!--------------------------->\n");}' >> `echo $HOME`/yosys/BitMake/test.c
+echo 'printf("<---------------------------Bitstream Upload Failed!--------------------------->\\n");}' >> `echo $HOME`/yosys/BitMake/test.c
 echo '}' >> `echo $HOME`/yosys/BitMake/test.c
 echo 'return 0;}' >> `echo $HOME`/yosys/BitMake/test.c
 gcc -Wall -o `echo $HOME`/yosys/BitMake/progtest `echo $HOME`/yosys/BitMake/test.c -lwiringPi
-sudo rm `echo $HOME`/yosys/BitMake/test.c
+sudo rm -f `echo $HOME`/yosys/BitMake/test.c
 sudo chmod +x `echo $HOME`/yosys/BitMake/progtest
-sudo cp /root/.yosys_show.dot `echo $dir`/`echo $parent`/`echo $parent`.dot
-sudo rm /root/.yosys_show.dot /root/.yosys_history /root/.yosys_show.dot.pid /root/.yosys_show.ps
-set -e
-sudo dot -T pdf `echo $dir`/`echo $parent`/`echo $parent`.dot -o `echo $dir`/`echo $parent`/`echo $parent`.pdf
+sudo dot -Tpdf `echo $HOME`/.yosys_show.dot -o `echo $dir`/`echo $parent`/`echo $parent`.pdf
 sudo killall -s SIGKILL xdot
 cd `echo $HOME`/yosys/BitMake
 sudo make v_fname=`echo $dir`/`echo $parent`/`echo $parent`
 cd `echo $dir`
 sudo `echo $HOME`/yosys/BitMake/./progtest
-sudo rm `echo $HOME`/yosys/BitMake/Makefile
-sudo rm `echo $HOME`/yosys/BitMake/progtest
-sudo rm `echo $HOME`/yosys/BitMake/ascript.sh
-sudo rm `echo $dir`/`echo $parent`/`echo $parent`.asc `echo $dir`/`echo $parent`/`echo $parent`.bin `echo $dir`/`echo $parent`/`echo $parent`.blif `echo $dir`/`echo $parent`/`echo $parent`.dot 
+sudo rm -f `echo $HOME`/yosys/BitMake/Makefile
+sudo rm -f `echo $HOME`/yosys/BitMake/progtest
+sudo rm -f `echo $HOME`/yosys/BitMake/ascript.sh
+sudo rm -f `echo $dir`/`echo $parent`/`echo $parent`.asc `echo $dir`/`echo $parent`/`echo $parent`.bin `echo $dir`/`echo $parent`/`echo $parent`.blif `echo $dir`/`echo $parent`/`echo $parent`.dot 
+sudo rm -f `echo $HOME`/.yosys_show.dot `echo $HOME`/.yosys_history `echo $HOME`/.yosys_show.dot.pid `echo $HOME`/.yosys_show.ps
 exec bash
 
 
