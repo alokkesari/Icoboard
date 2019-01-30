fc00fc612eef43d8120411ea7940af0db775ecb8
alokkesari <arkesari@iith.ac.in>
2019-01-30 12:24:38 +0530

    -y added

--- depinstall.sh
+++ depinstall.sh
@@ -1,18 +1,18 @@
 cd #HOME
-sudo apt-get install geany
+sudo apt-get install geany -y
 
 cd $HOME
 git clone git://git.drogon.net/wiringPi
 cd wiringPi && ./build
 
 cd $HOME
-sudo apt-get install subversion
+sudo apt-get install subversion -y
 svn co http://svn.clifford.at/handicraft/2015/icoprog
 cd icoprog && make install
 
-sudo apt-get install build-essential clang bison flex libreadline-dev
-sudo apt-get install gawk tcl8.6-dev libffi-dev git mercurial graphviz
-sudo apt-get install xdot pkg-config python python3 libftdi-dev
+sudo apt-get install build-essential clang bison flex libreadline-dev -y
+sudo apt-get install gawk tcl8.6-dev libffi-dev git mercurial graphviz -y
+sudo apt-get install xdot pkg-config python python3 libftdi-dev -y
 
 cd $HOME
 git clone https://github.com/cliffordwolf/icestorm.git icestorm
